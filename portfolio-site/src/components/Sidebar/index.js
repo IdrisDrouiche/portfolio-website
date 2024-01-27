a70608e132d69fb259e724518fef954a324d7903
IdrisDrouiche <idrisdrouiche@gmail.com>
2024-01-26 21:26:10 -0500

    added sidebar basics

--- portfolio-site/src/components/Sidebar/index.js
+++ portfolio-site/src/components/Sidebar/index.js
@@ -1,12 +1,19 @@
 import './index.scss'
-import { Link } from 'react-router-dom'
-import Logo from '../../assets/logo.jpg'
+import { Link, NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import Logo from '../../assets/images/logo.jpg'
 
 const Sidebar = () => (
   <div className="navBar">
     <Link className="logo" to="/">
       <img src={Logo} alt="logo" />
     </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="white"></FontAwesomeIcon>
+      </NavLink>
+    </nav>
   </div>
 )
 
