5f3541ac02dd83f818287c4a4bbfc7afae6163ff
IdrisDrouiche <idrisdrouiche@gmail.com>
2024-01-30 09:54:02 -0500

    home page

--- portfolio-site/src/components/Home/index.js
+++ portfolio-site/src/components/Home/index.js
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom'
+import './index.scss'
+
 const Home = () => {
   return (
     <div className="container homePage">
       <div className="textArea">
         <h1>Idris Drouiche</h1>
+        <h2>
+          Software Engineer / Full Stack Developer / Cyber Security Analyst
+        </h2>
+        <Link to="/contact" className="contactButton">
+          Contact Me
+        </Link>
       </div>
     </div>
   )
